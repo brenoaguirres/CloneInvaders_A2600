cb4399763c07d05a97a0c7825a40a62a3064d3d8
brenoaguirres <142112771+brenoaguirres@users.noreply.github.com>
2025-04-28 18:04:54 -0300

    Update ObjectPool.cs

--- Assets/Scripts/Tools/ObjectPool/ObjectPool.cs
+++ Assets/Scripts/Tools/ObjectPool/ObjectPool.cs
@@ -1,9 +1,53 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Adventure.Tools.ObjectPool
 {
-    public class ObjectPool<T> : MonoBehaviour where T : MonoBehaviour
+    public class ObjectPool<T> where T : MonoBehaviour
     {
+        #region FIELDS
+        private List<T> _objects = new();
+        #endregion
 
+        #region PROPERTIES
+        public List<T> Objects => _objects;
+        #endregion
+
+        #region METHODS
+        public void Add(T obj)
+        {
+            if (!_objects.Contains(obj))
+            _objects.Add(obj);
+        }
+
+        public void Remove(T obj)
+        {
+            if (_objects.Contains(obj))
+                _objects.Remove(obj);
+        }
+
+        public void Clear()
+        {
+            _objects.Clear();
+        }
+
+        public T GetFirst()
+        {
+            if (_objects[0] == null) return null;
+            return _objects[0];
+        }
+
+        public T GetLast()
+        {
+            if (_objects[^1] == null) return null;
+            return _objects[^1];
+        }
+
+        public T Pick()
+        {
+            if (_objects.Count <= 0) return null;
+            return _objects[UnityEngine.Random.Range(0, _objects.Count - 1)];
+        }
+        #endregion
     }
 }
